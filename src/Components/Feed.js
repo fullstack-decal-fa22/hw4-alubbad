51f6f8289fe4c56b06ffe1a87887cba249296daa
Aya Lubbad <walaa@Ayas-MacBook-Pro.local>
2022-10-25 00:11:12 -0700

    final

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -6,16 +6,20 @@ import Block from './Block';
 const Feed = () => {
     /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
     // Refer to Hint 2 for more help!
-
     /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
-
+    const [posts, setPosts] = useState([]); // TODO: edit this variable
+    const updatePosts= (newPost)=> {
+        const newPosts = [...posts];
+        newPosts.push(newPost);
+        setPosts(newPosts);
+    }
     return (
         <div>
-            <Menu></Menu>
+            <Menu updatePosts = {updatePosts}></Menu>
 
-            {/* Below is where all of your Blocks should render! */}
-            {posts}
+            {posts.map((p)=>(
+                <Block color ={p.color} caption={p.caption}/>
+            ))}
         </div>
     );
 }
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,19 @@
+/*import { getQueriesForElement } from '@testing-library/react';*/
 import React from 'react';
+import Color from './Color';
 /* Add any imports you think you might need here! */
 
-const Menu = () => { 
+const Menu = (props) => { 
+  const handleClick = (c) => {
+    props.updatePosts({ color: c, caption: c});
+  }
 
     return (
       <div className="colorOptions">
-          {/* TODO */}
+          <Color color = "green" handleClick = {handleClick}/>
+          <Color color = "blue" handleClick = {handleClick}/>
+          <Color color = "red" handleClick = {handleClick}/>
+          <Color color = "pink" handleClick = {handleClick}/>
       </div>
     );
 }
